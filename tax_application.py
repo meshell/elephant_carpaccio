a16cf580819b71bac05b0c830b5a9f31ff85a7a7
Michel Estermann <estermann.michel@gmail.com>
2018-06-18 11:24:15 +0200

    Calculate order value.

--- tax_application.py
+++ tax_application.py
@@ -9,14 +9,29 @@
 import argparse
 
 
+def calculate_price(number_of_items, item_price):
+    return number_of_items * item_price
+
 def parse_arguments():
     """
     Setup the argument parsers for handling CLI arguments and return a tuple
     with (parsing results, parser).
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--items", type=int, help='Number of items', action='store', required=True)
-    parser.add_argument("-p", "--price", type=float, help='item price', action='store', required=True)
+    parser.add_argument(
+        "-i",
+        "--items",
+        type=int,
+        help='Number of items',
+        action='store',
+        required=True)
+    parser.add_argument(
+        "-p",
+        "--price",
+        type=float,
+        help='item price',
+        action='store',
+        required=True)
 
     return parser.parse_args(), parser
 
@@ -28,8 +43,9 @@ def main():
     """
     args, parser = parse_arguments()
 
-    print("Number of items: {}. Item price: {} U$".format(args.items, args.price))
-    
+    order_value = calculate_price(args.items, args.price)
+    print("Number of items: {}. Item price: {} U$ --> Order value: {} U$".format(args.items, args.price, order_value))
+
 
 if __name__ == '__main__':
     main()
